4b7756d36419ea9a9fe14958f7c0f25e65167d1f
Emre Turkay <emreturkay@gmail.com>
2015-04-27 18:35:19 +0300

    Added custom elements from Polymer.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.use("/bower_components", express.static(__dirname + "/bower_components"));
 
 io.on('connection', function(socket) {
     socket.on('chat message', function(msg) {
